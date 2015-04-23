e6e82b695975840635c280c05534836ac6debd0c
ikhakoo <ikhakoo@gmail.com>
2015-04-23 18:03:21 -0400

    added column capacity to restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423215622) do
+ActiveRecord::Schema.define(version: 20150423220253) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150423215622) do
     t.integer  "rating"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "capacity"
   end
 
   create_table "users", force: :cascade do |t|
