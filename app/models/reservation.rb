8443405e79b5e2969e261305b770c0323505e0ba
ikhakoo <ikhakoo@gmail.com>
2015-04-23 18:31:34 -0400

    sign up page and sessions is working

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,4 @@
 class Reservation < ActiveRecord::Base
+	belongs_to :restaurant
+	belong_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   root 'restaurants#index'
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :restaurants
+  resources :reservations
   
 end
