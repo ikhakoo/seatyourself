77c2de38a5207d5acedbb59c89ba3a05422cb034
ikhakoo <ikhakoo@gmail.com>
2015-04-24 10:52:04 -0400

    cleaned up user mailer and updated gem file for pry and better errors

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
  
   def conf_email(user)
     @user = user
-    @url  = 'http://seatyourself.git'
     mail(to: @user.email, subject: 'Reservation Confirmation')
   end
 end
